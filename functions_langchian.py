90391f2142168487ff211674ab291e9ed66d6579
Qammarbhat <qammarbhat@bgsbu.ac.in>
2023-11-02 12:08:51 +0530

    key fix

--- functions_langchian.py
+++ functions_langchian.py
@@ -1,8 +1,11 @@
 from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+# api_key = os.getenv("OPENAI_API_KEY")
+# print(api_key)
 llm = OpenAI(temperature = 0.1)
 
 def csv_reader(filepath,user_question ):
